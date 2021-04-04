068eea70f98934e8643ed66473214efe7a48b02c
ysfdogan <81864744+ysfdogan@users.noreply.github.com>
2021-04-04 18:13:12 +0300

    Update MyGitClass1.java
    github.com daki yeni versiyonumu olusturuyorum

--- src/mygitpackage1/MyGitClass1.java
+++ src/mygitpackage1/MyGitClass1.java
@@ -5,6 +5,7 @@ public class MyGitClass1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Ilk deneyim olacak insaallah");
+		System.out.println("uzak sunucudaki kodum");//Github koddaki yeni kodum
 
 	}
 
