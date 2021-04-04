1a6f8d397efcc8dbf68db459e636d0497672310e
Your Name <you@example.com>
2021-04-04 13:49:29 +0300

    version2

---
+++ src/mygitpackage1/MyGitClass1.java
@@ -0,0 +1,11 @@
+package mygitpackage1;
+
+public class MyGitClass1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ilk deneyim olacak insaallah");
+
+	}
+
+}
---
+++ src/mygitpackage1/MyGitClass2.java
@@ -0,0 +1,11 @@
+package mygitpackage1;
+
+public class MyGitClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("Yapamiyorum");
+		System.out.println("Olacak insaallah");
+
+	}
+
+}
